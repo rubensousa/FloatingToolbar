35bde8367b945d16a143c78a10121bfb5722279f
ruben <rubensousa.mieti@gmail.com>
2016-08-04 18:37:01 +0100

    Add sample with resizable window due to text input

--- sample/src/main/java/com/github/rubensousa/floatingtoolbar/sample/DetailActivity.java
+++ sample/src/main/java/com/github/rubensousa/floatingtoolbar/sample/DetailActivity.java
@@ -22,21 +22,76 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.github.rubensousa.floatingtoolbar.FloatingToolbar;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements FloatingToolbar.ItemClickListener {
+
+    private FloatingActionButton mFabAppBar;
+    private FloatingActionButton mFab;
+    private AppBarLayout mAppBar;
+    private FloatingToolbar mFloatingToolbar;
+    private boolean mShowingFromAppBar;
+    private boolean mShowingFromNormal;
+
+    // 808100900
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_activity);
 
-        AppBarLayout appbar = (AppBarLayout) findViewById(R.id.appbar);
-        FloatingToolbar floatingToolbar = (FloatingToolbar) findViewById(R.id.floatingToolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        floatingToolbar.attachFab(fab);
-        floatingToolbar.attachAppBarLayout(appbar);
+        mAppBar = (AppBarLayout) findViewById(R.id.appbar);
+        mFloatingToolbar = (FloatingToolbar) findViewById(R.id.floatingToolbar);
+        mFabAppBar = (FloatingActionButton) findViewById(R.id.fab);
+        mFab = (FloatingActionButton) findViewById(R.id.fab2);
+
+        // Don't handle fab click since we'll have 2 of them
+        mFloatingToolbar.handleFabClick(false);
+        mFloatingToolbar.setClickListener(this);
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAppBar.setExpanded(false, true);
+                mFloatingToolbar.attachFab(mFab);
+                mFloatingToolbar.show();
+                mShowingFromNormal = true;
+            }
+        });
+
+        mFabAppBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mFloatingToolbar.isShowing()) {
+                    mFab.hide();
+                    mFloatingToolbar.attachAppBarLayout(mAppBar);
+                    mFloatingToolbar.attachFab(mFabAppBar);
+                    mFloatingToolbar.show();
+                    mShowingFromAppBar = true;
+                }
+            }
+        });
     }
 
+    @Override
+    public void onItemClick(MenuItem item) {
+        if (mShowingFromAppBar) {
+            mFab.show();
+        }
+
+        if (mShowingFromNormal) {
+            mFabAppBar.show();
+        }
+
+        mShowingFromAppBar = false;
+        mShowingFromNormal = false;
+    }
+
+    @Override
+    public void onItemLongClick(MenuItem item) {
+
+    }
 }
