d44ff452dbcf26c32638f32715f7d4fc6559a1ef
ruben <rubensousa.mieti@gmail.com>
2016-12-17 19:16:02 +0000

    Remove unused import

--- floatingtoolbar/src/main/java/com/github/rubensousa/floatingtoolbar/FloatingToolbar.java
+++ floatingtoolbar/src/main/java/com/github/rubensousa/floatingtoolbar/FloatingToolbar.java
@@ -41,7 +41,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.widget.Toast;
 
 import java.util.ArrayList;
