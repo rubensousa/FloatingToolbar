5227a6f657e1aeb970bd466ff9ed70637850fcc2
ruben <rubensousa.mieti@gmail.com>
2017-02-28 20:59:30 +0000

    Fix merge issue

--- floatingtoolbar/src/main/java/com/github/rubensousa/floatingtoolbar/FloatingToolbar.java
+++ floatingtoolbar/src/main/java/com/github/rubensousa/floatingtoolbar/FloatingToolbar.java
@@ -119,7 +119,7 @@ public class FloatingToolbar extends LinearLayoutCompat implements View.OnClickL
         int customView = a.getResourceId(R.styleable.FloatingToolbar_floatingCustomView, 0);
 
         if (customView != 0) {
-            mCustomView = LayoutInflater.from(context).inflate(customView, this, true);
+            mCustomView = LayoutInflater.from(context).inflate(customView, this, false);
         }
 
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
@@ -129,6 +129,7 @@ public class FloatingToolbar extends LinearLayoutCompat implements View.OnClickL
         }
 
         if (mCustomView != null) {
+            addView(mCustomView);
             mAnimator.setContentView(mCustomView);
         }
 
@@ -516,12 +517,18 @@ public class FloatingToolbar extends LinearLayoutCompat implements View.OnClickL
 
     void dispatchShow() {
         mShowing = true;
-        mAnimating = true;
 
-        mAnimator.show();
-
-        for (MorphListener morphListener : mMorphListeners) {
-            morphListener.onMorphStart();
+        if (getWidth() == 0 && getHeight() == 0) {
+            setVisibility(View.VISIBLE);
+            if (mFab != null) {
+                mFab.setVisibility(View.INVISIBLE);
+            }
+        } else {
+            mAnimating = true;
+            mAnimator.show();
+            for (MorphListener morphListener : mMorphListeners) {
+                morphListener.onMorphStart();
+            }
         }
     }
 
